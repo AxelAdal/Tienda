3a7349b8bc7e1dd8c9df7b5110265109895576b4
Axel <adalvarez@scl.edu.gt>
2023-07-01 12:52:48 -0600

    Inicio

---
+++ js-data/datos-DC.js
@@ -0,0 +1,72 @@
+let coleDC   = [
+    {
+        nombre: "Figura De Accion Funko POP #1236 DC Comics Peacemaker Eagly",
+        Precio: "Q159.90",
+        descripcion: "Figuras Coleccionables",
+        img: "url",
+        existencia: true
+    },
+    {
+        nombre: "Figura De Accion Funko POP #438 DC Comics Juntice League Starfire",
+        Precio: "Q169.90",
+        descripcion: "Figuras Coleccionables",
+        img: "url",
+        existencia: true
+    },
+    {
+        nombre: "Figura De Accion Funko POP Heroes DC #412 Bane Glow Exclusive",
+        Precio: "Q159.90",
+        descripcion: "Figuras Coleccionables",
+        img: "url",
+        existencia: true
+    },
+    {
+        nombre: "Figura De Accion Funko POP #1234 DC Comics Black Adam Cyclone",
+        Precio: "Q159.90",
+        descripcion: "Coleccionables y Juguetes",
+        img: "url",
+        existencia: true
+    },
+    {
+        nombre: "Figura De Accion Funko POP #1233 DC Comics Peacemaker En Ropa Interior",
+        Precio: "Q159.90",
+        descripcion: "Figuras Coleccionables",
+        img: "url",
+        existencia: true
+    },
+    {
+        nombre: "Figura De Accion Funko POP #435 DC Justice League Black Orchid Edicion Especial Dia De La Tierra",
+        Precio: "Q107.46",
+        descripcion: "Figuras Coleccionables",
+        img: "url",
+        existencia: true
+    },
+    {
+        nombre: "Figura De Accion Funko POP #431 Heroes Wonder Woman Flaspoint",
+        Precio: "Q159.90",
+        descripcion: "Figuras Coleccionables",
+        img: "url",
+        existencia: true
+    },
+    {
+        nombre: "The Suicide Squad Harley Quinn Damaged Dress Funko Pop",
+        Precio: "Q102.18",
+        descripcion: "Figuras Coleccionables",
+        img: "url",
+        existencia: true
+    },
+    {
+        nombre: "Comprar Funko POP Batman (Michael Keaton) 1339 DC Comics The Flash",
+        Precio: "Q159.90",
+        descripcion: "Figuras Coleccionables",
+        img: "url",
+        existencia: true
+    },
+    {
+        nombre: "Funko POP! Movies: DC Justice League Superman Toy Figure",
+        Precio: "Q114.90",
+        descripcion: "Figuras Coleccionables",
+        img: "url",
+        existencia: true
+    }
+]
---
+++ js-data/datos-disney.js
@@ -0,0 +1,72 @@
+let disney   = [
+    {
+        nombre: "Funko Pop Disney: Merida Collectible Vinyl Figure",
+        Precio: "Q289.00",
+        descripcion: "Figuras Coleccionables",
+        img: "url",
+        existencia: true
+    },
+    {
+        nombre: "Funko Pop Disney: Merida Collectible Vinyl Figure",
+        Precio: "Q239.90",
+        descripcion: "Figuras Coleccionables",
+        img: "url",
+        existencia: true
+    },
+    {
+        nombre: "Funko Pop! Train: Disney 100 - Mickey in Steamboat Car, Mickey Mouse, Amazon Exclusive",
+        Precio: "Q219.90",
+        descripcion: "Figuras Coleccionables",
+        img: "url",
+        existencia: true
+    },
+    {
+        nombre: "Funko Pop! Disney: Walt Disney World 50th Anniversary - Aloha Mickey",
+        Precio: "Q189.90",
+        descripcion: "Coleccionables y Juguetes",
+        img: "url",
+        existencia: true
+    },
+    {
+        nombre: "Funko Pop! Disney Classics: Mickey and Friends - Pluto",
+        Precio: "Q189.90",
+        descripcion: "Figuras Coleccionables",
+        img: "url",
+        existencia: true
+    },
+    {
+        nombre: "Funko Pop! Disney: Disney 100 - Aurora",
+        Precio: "Q179.46",
+        descripcion: "Figuras Coleccionables",
+        img: "url",
+        existencia: true
+    },
+    {
+        nombre: "Funko Pop! VHS Cover: Disney - Monsters, Inc., Boo with Hard Hat, Amazon Exclusive",
+        Precio: "Q309.90",
+        descripcion: "Figuras Coleccionables",
+        img: "url",
+        existencia: true
+    },
+    {
+        nombre: "Funko Pop! Jumbo: Disney Villains - 10 Maleficent Dragon",
+        Precio: "Q419.18",
+        descripcion: "Figuras Coleccionables",
+        img: "url",
+        existencia: true
+    },
+    {
+        nombre: "Funko Pop! Art Cover: Disney 100 - Oswald The Lucky Rabbit",
+        Precio: "Q339.90",
+        descripcion: "Figuras Coleccionables",
+        img: "url",
+        existencia: true
+    },
+    {
+        nombre: "Funko Pop! Disney: Dumbo in Bubble Bath #1195",
+        Precio: "Q209.90",
+        descripcion: "Figuras Coleccionables",
+        img: "url",
+        existencia: true
+    }
+]
