f074cb8a697692f5e15241df0c6ae108889f029c
Axel <adalvarez@scl.edu.gt>
2023-07-02 13:22:09 -0600

    Buscador links

--- js-data/datos-DC.js
+++ js-data/datos-DC.js
@@ -1,151 +1,152 @@
-let coleDC   = [
-    {
-        nombre: "Figura De Accion Funko POP #1236 DC Comics Peacemaker Eagly",
-        Precio: "Q159.90",
-        descripcion: "Figuras Coleccionables ",
-        franquicia: "DC",
-        img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/Peacemaker%20Eagly.png",
-        existencia: true
-    },
-    {
-        nombre: "Figura De Accion Funko POP #438 DC Comics Juntice League Starfire",
-        Precio: "Q169.90",
-        franquicia: "DC",
-        descripcion: "Figuras Coleccionables",
-        img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/League%20Starfire.jpg",
-        existencia: true
-    },
-    {
-        nombre: "Figura De Accion Funko POP Heroes DC #412 Bane Glow Exclusive",
-        Precio: "Q159.90",
-        franquicia: "DC",
-        descripcion: "Figuras Coleccionables",
-        img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/Bane%20Glow.jpg",
-        existencia: true
-    },
-    {
-        nombre: "Figura De Accion Funko POP #1234 DC Comics Black Adam Cyclone",
-        Precio: "Q159.90",
-        franquicia: "DC",
-        descripcion: "Coleccionables y Juguetes",
-        img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/Cyclone.jpg",
-        existencia: true
-    },
-    {
-        nombre: "Figura De Accion Funko POP #1233 DC Comics Peacemaker En Ropa Interior",
-        Precio: "Q159.90",
-        franquicia: "DC",
-        descripcion: "Figuras Coleccionables",
-        img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/peacemaker.jpg",
-        existencia: true
-    },
-    {
-        nombre: "Figura De Accion Funko POP #435 DC Justice League Black Orchid Edicion Especial Dia De La Tierra",
-        Precio: "Q107.46",
-        franquicia: "DC",
-        descripcion: "Figuras Coleccionables",
-        img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/Orchid.jpg",
-        existencia: true
-    },
-    {
-        nombre: "Figura De Accion Funko POP #431 Heroes Wonder Woman Flaspoint",
-        Precio: "Q159.90",
-        franquicia: "DC",
-        descripcion: "Figuras Coleccionables",
-        img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/Wonder%20Woman%20Flaspoint.jpeg",
-        existencia: true
-    },
-    {
-        nombre: "The Suicide Squad Harley Quinn Damaged Dress Funko Pop",
-        Precio: "Q102.18",
-        franquicia: "DC",
-        descripcion: "Figuras Coleccionables",
-        img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/Harley%20Quinn.jpg",
-        existencia: true
-    },
-    {
-        nombre: "Comprar Funko POP Batman (Michael Keaton) 1339 DC Comics The Flash",
-        Precio: "Q159.90",
-        franquicia: "DC",
-        descripcion: "Figuras Coleccionables",
-        img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/Batman.jpg",
-        existencia: true
-    },
-    {
-        nombre: "Funko POP! Movies: DC Justice League Superman Toy Figure",
-        Precio: "Q114.90",
-        franquicia: "DC",
-        descripcion: "Figuras Coleccionables",
-        img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/Superman.jpg",
-        existencia: true
-    },
-]
-const contenedorProductos = document.getElementById('cajasDC');
+let coleDC = [
+        {
+            nombre: "Figura De Accion Funko POP #1236 DC Comics Peacemaker Eagly",
+            Precio: "Q159.90",
+            descripcion: "Figuras Coleccionables ",
+            franquicia: "DC",
+            img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/Peacemaker%20Eagly.png",
+            existencia: true
+        },
+        {
+            nombre: "Figura De Accion Funko POP #438 DC Comics Juntice League Starfire",
+            Precio: "Q169.90",
+            franquicia: "DC",
+            descripcion: "Figuras Coleccionables",
+            img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/League%20Starfire.jpg",
+            existencia: true
+        },
+        {
+            nombre: "Figura De Accion Funko POP Heroes DC #412 Bane Glow Exclusive",
+            Precio: "Q159.90",
+            franquicia: "DC",
+            descripcion: "Figuras Coleccionables",
+            img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/Bane%20Glow.jpg",
+            existencia: true
+        },
+        {
+            nombre: "Figura De Accion Funko POP #1234 DC Comics Black Adam Cyclone",
+            Precio: "Q159.90",
+            franquicia: "DC",
+            descripcion: "Coleccionables y Juguetes",
+            img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/Cyclone.jpg",
+            existencia: true
+        },
+        {
+            nombre: "Figura De Accion Funko POP #1233 DC Comics Peacemaker En Ropa Interior",
+            Precio: "Q159.90",
+            franquicia: "DC",
+            descripcion: "Figuras Coleccionables",
+            img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/peacemaker.jpg",
+            existencia: true
+        },
+        {
+            nombre: "Figura De Accion Funko POP #435 DC Justice League Black Orchid Edicion Especial Dia De La Tierra",
+            Precio: "Q107.46",
+            franquicia: "DC",
+            descripcion: "Figuras Coleccionables",
+            img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/Orchid.jpg",
+            existencia: true
+        },
+        {
+            nombre: "Figura De Accion Funko POP #431 Heroes Wonder Woman Flaspoint",
+            Precio: "Q159.90",
+            franquicia: "DC",
+            descripcion: "Figuras Coleccionables",
+            img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/Wonder%20Woman%20Flaspoint.jpeg",
+            existencia: true
+        },
+        {
+            nombre: "The Suicide Squad Harley Quinn Damaged Dress Funko Pop",
+            Precio: "Q102.18",
+            franquicia: "DC",
+            descripcion: "Figuras Coleccionables",
+            img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/Harley%20Quinn.jpg",
+            existencia: true
+        },
+        {
+            nombre: "Comprar Funko POP Batman (Michael Keaton) 1339 DC Comics The Flash",
+            Precio: "Q159.90",
+            franquicia: "DC",
+            descripcion: "Figuras Coleccionables",
+            img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/Batman.jpg",
+            existencia: true
+        },
+        {
+            nombre: "Funko POP! Movies: DC Justice League Superman Toy Figure",
+            Precio: "Q114.90",
+            franquicia: "DC",
+            descripcion: "Figuras Coleccionables",
+            img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/Superman.jpg",
+            existencia: true
+        },
+    ]
+    const contenedorProductos = document.getElementById('cajasDC');
     contenedorProductos.classList.add('cajasDC');
-
-    coleDC.forEach(producto => {
-        const divProducto = document.createElement('div');
-        divProducto.classList.add('producto');
-
-        const divNombre = document.createElement('div');
-        divNombre.classList.add('nombre');
-        divNombre.textContent = producto.nombre;
-        divProducto.appendChild(divNombre);
-
-        const divPrecio = document.createElement('div');
-        divPrecio.classList.add('precio');
-        divPrecio.textContent = 'Precio: ' + producto.Precio;
-        divProducto.appendChild(divPrecio);
-
-        const divDescripcion = document.createElement('div');
-        divDescripcion.classList.add('descripcion');
-        divDescripcion.textContent = producto.descripcion;
-        divProducto.appendChild(divDescripcion);
-
-        const divFranquicia = document.createElement('div');
-        divFranquicia.classList.add('franquicia');
-        divFranquicia.textContent = 'Franquicia: ' + producto.franquicia;
-        divProducto.appendChild(divFranquicia);
-
-        const imagen = document.createElement('img');
-        imagen.classList.add('imagen');
-        imagen.src = producto.img;
-        divProducto.appendChild(imagen);
-
-        const btnMostrar = document.createElement('button');
-        btnMostrar.textContent = 'Ver Producto';
-        btnMostrar.addEventListener('click', () => mostrarEnGrande(producto));
-        divProducto.appendChild(btnMostrar);
-
-        contenedorProductos.appendChild(divProducto);
+    
+    coleDC.forEach((producto, index) => {
+      const divProducto = document.createElement('div');
+      divProducto.classList.add('producto');
+      divProducto.id = 'producto-' + index; // Asignar un ID único basado en el índice
+    
+      const divNombre = document.createElement('div');
+      divNombre.classList.add('nombre');
+      divNombre.textContent = producto.nombre;
+      divProducto.appendChild(divNombre);
+    
+      const divPrecio = document.createElement('div');
+      divPrecio.classList.add('precio');
+      divPrecio.textContent = 'Precio: ' + producto.Precio;
+      divProducto.appendChild(divPrecio);
+    
+      const divDescripcion = document.createElement('div');
+      divDescripcion.classList.add('descripcion');
+      divDescripcion.textContent = producto.descripcion;
+      divProducto.appendChild(divDescripcion);
+    
+      const divFranquicia = document.createElement('div');
+      divFranquicia.classList.add('franquicia');
+      divFranquicia.textContent = 'Franquicia: ' + producto.franquicia;
+      divProducto.appendChild(divFranquicia);
+    
+      const imagen = document.createElement('img');
+      imagen.classList.add('imagen');
+      imagen.src = producto.img;
+      divProducto.appendChild(imagen);
+    
+      const btnMostrar = document.createElement('button');
+      btnMostrar.textContent = 'Ver Producto';
+      btnMostrar.addEventListener('click', () => mostrarEnGrande(producto));
+      divProducto.appendChild(btnMostrar);
+    
+      contenedorProductos.appendChild(divProducto);
     });
-
+    
     function mostrarEnGrande(producto) {
-        const contenedorModal = document.createElement('div');
-        contenedorModal.classList.add('modal');
-
-        const modalContenido = document.createElement('div');
-        modalContenido.classList.add('modal-contenido');
-
-        const imagenModal = document.createElement('img');
-        imagenModal.classList.add('imagen-modal');
-        imagenModal.src = producto.img;
-        modalContenido.appendChild(imagenModal);
-
-        const textoModal = document.createElement('div');
-        textoModal.classList.add('texto-modal');
-        textoModal.innerHTML = `
-            <h2>${producto.nombre}</h2>
-            <p><strong>Precio:</strong> ${producto.Precio}</p>
-            <p><strong>Descripción:</strong> ${producto.descripcion}</p>
-            <p><strong>Franquicia:</strong> ${producto.franquicia}</p>
-        `;
-        modalContenido.appendChild(textoModal);
-
-        contenedorModal.appendChild(modalContenido);
-        document.body.appendChild(contenedorModal);
-
-        contenedorModal.addEventListener('click', () => {
-            contenedorModal.remove();
-        });
-}
+      const contenedorModal = document.createElement('div');
+      contenedorModal.classList.add('modal');
+    
+      const modalContenido = document.createElement('div');
+      modalContenido.classList.add('modal-contenido');
+    
+      const imagenModal = document.createElement('img');
+      imagenModal.classList.add('imagen-modal');
+      imagenModal.src = producto.img;
+      modalContenido.appendChild(imagenModal);
+    
+      const textoModal = document.createElement('div');
+      textoModal.classList.add('texto-modal');
+      textoModal.innerHTML = `
+        <h2>${producto.nombre}</h2>
+        <p><strong>Precio:</strong> ${producto.Precio}</p>
+        <p><strong>Descripción:</strong> ${producto.descripcion}</p>
+        <p><strong>Franquicia:</strong> ${producto.franquicia}</p>
+      `;
+      modalContenido.appendChild(textoModal);
+    
+      contenedorModal.appendChild(modalContenido);
+      document.body.appendChild(contenedorModal);
+    
+      contenedorModal.addEventListener('click', () => {
+        contenedorModal.remove();
+      });
+    }
--- js-data/todofunco-data.js
+++ js-data/todofunco-data.js
@@ -4,6 +4,7 @@ let todofunco   = [
         Precio: "Q159.90",
         descripcion: "Figuras Coleccionables ",
         franquicia: "DC",
+        enlace:"./index2.html#producto-0",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/Peacemaker%20Eagly.png",
         existencia: true
     },
@@ -11,6 +12,7 @@ let todofunco   = [
         nombre: "Figura De Accion Funko POP #438 DC Comics Juntice League Starfire",
         Precio: "Q169.90",
         franquicia: "DC",
+        enlace:"./index2.html#producto-1",
         descripcion: "Figuras Coleccionables",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/League%20Starfire.jpg",
         existencia: true
@@ -19,6 +21,7 @@ let todofunco   = [
         nombre: "Figura De Accion Funko POP Heroes DC #412 Bane Glow Exclusive",
         Precio: "Q159.90",
         franquicia: "DC",
+        enlace:"./index2.html#producto-2",
         descripcion: "Figuras Coleccionables",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/Bane%20Glow.jpg",
         existencia: true
@@ -27,6 +30,7 @@ let todofunco   = [
         nombre: "Figura De Accion Funko POP #1234 DC Comics Black Adam Cyclone",
         Precio: "Q159.90",
         franquicia: "DC",
+        enlace:"./index2.html#producto-3",
         descripcion: "Coleccionables y Juguetes",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/Cyclone.jpg",
         existencia: true
@@ -35,6 +39,7 @@ let todofunco   = [
         nombre: "Figura De Accion Funko POP #1233 DC Comics Peacemaker En Ropa Interior",
         Precio: "Q159.90",
         franquicia: "DC",
+        enlace:"./index2.html#producto-4",
         descripcion: "Figuras Coleccionables",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/peacemaker.jpg",
         existencia: true
@@ -43,6 +48,7 @@ let todofunco   = [
         nombre: "Figura De Accion Funko POP #435 DC Justice League Black Orchid Edicion Especial Dia De La Tierra",
         Precio: "Q107.46",
         franquicia: "DC",
+        enlace:"./index2.html#producto-5",
         descripcion: "Figuras Coleccionables",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/Orchid.jpg",
         existencia: true
@@ -51,6 +57,7 @@ let todofunco   = [
         nombre: "Figura De Accion Funko POP #431 Heroes Wonder Woman Flaspoint",
         Precio: "Q159.90",
         franquicia: "DC",
+        enlace:"./index2.html#producto-6",
         descripcion: "Figuras Coleccionables",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/Wonder%20Woman%20Flaspoint.jpeg",
         existencia: true
@@ -59,6 +66,7 @@ let todofunco   = [
         nombre: "The Suicide Squad Harley Quinn Damaged Dress Funko Pop",
         Precio: "Q102.18",
         franquicia: "DC",
+        enlace:"./index2.html#producto-7",
         descripcion: "Figuras Coleccionables",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/Harley%20Quinn.jpg",
         existencia: true
@@ -67,6 +75,7 @@ let todofunco   = [
         nombre: "Comprar Funko POP Batman (Michael Keaton) 1339 DC Comics The Flash",
         Precio: "Q159.90",
         franquicia: "DC",
+        enlace:"./index2.html#producto-8",
         descripcion: "Figuras Coleccionables",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/Batman.jpg",
         existencia: true
@@ -75,6 +84,7 @@ let todofunco   = [
         nombre: "Funko POP! Movies: DC Justice League Superman Toy Figure",
         Precio: "Q114.90",
         franquicia: "DC",
+        enlace:"./index2.html#producto-9",
         descripcion: "Figuras Coleccionables",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-DC/Superman.jpg",
         existencia: true
@@ -87,6 +97,7 @@ let todofunco   = [
         nombre: "Funko Pop Disney: Blanca Nieves Collectible Figure",
         Precio: "Q289.00",
         Franquicia: "Disney",
+        enlace:"./index4.html#producto-0",
         descripcion: "Figuras Coleccionables",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-disney/BlancaN.jpg",
         existencia: true
@@ -95,6 +106,7 @@ let todofunco   = [
         nombre: "Funko Pop Disney: Merida Collectible Vinyl Figure",
         Precio: "Q239.90",
         Franquicia: "Disney",
+        enlace:"./index4.html#producto-1",
         descripcion: "Figuras Coleccionables",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-disney/Merida.jpg",
         existencia: true
@@ -103,6 +115,7 @@ let todofunco   = [
         nombre: "Funko Pop! Train: Disney 100 - Mickey in Steamboat Car, Mickey Mouse, Amazon Exclusive",
         Precio: "Q219.90",
         Franquicia: "Disney",
+        enlace:"./index4.html#producto-2",
         descripcion: "Figuras Coleccionables",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-disney/Mickeycar.jpg",
         existencia: true
@@ -111,6 +124,7 @@ let todofunco   = [
         nombre: "Funko Pop! Disney: Walt Disney World 50th Anniversary - Aloha Mickey",
         Precio: "Q189.90",
         Franquicia: "Disney",
+        enlace:"./index4.html#producto-3",
         descripcion: "Coleccionables y Juguetes",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-disney/mickey50ani.jpg",
         existencia: true
@@ -119,6 +133,7 @@ let todofunco   = [
         nombre: "Funko Pop! Disney Classics: Mickey and Friends - Pluto",
         Precio: "Q189.90",
         Franquicia: "Disney",
+        enlace:"./index4.html#producto-4",
         descripcion: "Figuras Coleccionables",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-disney/Pluto.jpg",
         existencia: true
@@ -127,6 +142,7 @@ let todofunco   = [
         nombre: "Funko Pop! Disney: Disney 100 - Aurora",
         Precio: "Q179.46",
         Franquicia: "Disney",
+        enlace:"./index4.html#producto-5",
         descripcion: "Figuras Coleccionables",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-disney/aurora.jpg",
         existencia: true
@@ -135,6 +151,7 @@ let todofunco   = [
         nombre: "Funko Pop! VHS Cover: Disney - Monsters, Inc., Boo with Hard Hat, Amazon Exclusive",
         Precio: "Q309.90",
         Franquicia: "Disney",
+        enlace:"./index4.html#producto-6",
         descripcion: "Figuras Coleccionables",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-disney/boo.jpg",
         existencia: true
@@ -143,6 +160,7 @@ let todofunco   = [
         nombre: "Funko Pop! Jumbo: Disney Villains - 10 Maleficent Dragon",
         Precio: "Q419.18",
         Franquicia: "Disney",
+        enlace:"./index4.html#producto-7",
         descripcion: "Figuras Coleccionables",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-disney/Malefica.jpg",
         existencia: true
@@ -151,6 +169,7 @@ let todofunco   = [
         nombre: "Funko Pop! Art Cover: Disney 100 - Oswald The Lucky Rabbit",
         Precio: "Q339.90",
         Franquicia: "Disney",
+        enlace:"./index4.html#producto-8",
         descripcion: "Figuras Coleccionables",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-disney/Oswald%20The%20Lucky%20Rabbit.jpg",
         existencia: true
@@ -159,6 +178,7 @@ let todofunco   = [
         nombre: "Funko Pop! Disney: Dumbo in Bubble Bath #1195",
         Precio: "Q209.90",
         Franquicia: "Disney",
+        enlace:"./index4.html#producto-9",
         descripcion: "Figuras Coleccionables",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-disney/dumbo.jpg",
         existencia: true
@@ -171,6 +191,7 @@ let todofunco   = [
         nombre: "Spiderman No Way Home",
         Precio: "Q200.00",
         Franquicia:"Marvel",
+        enlace:"./index3.html#producto-0",
         descripcion: "Funko Pop Spiderman No Way Home Sin Mascara",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-marvel/spider-man-No-way-1.png",
         existencia: true
@@ -179,6 +200,7 @@ let todofunco   = [
         nombre: "Marvel What If…? Zolavision",
         Precio: "Q189.90",
         Franquicia:"Marvel",
+        enlace:"./index3.html#producto-1",
         descripcion: "Figuras Coleccionables",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-marvel/zolavision.jpg",
         existencia: true
@@ -188,6 +210,7 @@ let todofunco   = [
         Precio: "Q314.65",
         Franquicia:"Marvel",
         descripcion: "Figuras Coleccionables",
+        enlace:"./index3.html#producto-2",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-marvel/Infinity%20Killmonger.jpg",
         existencia: true
     },
@@ -196,6 +219,7 @@ let todofunco   = [
         Precio: "Q479.90",
         Franquicia:"Marvel",
         descripcion: "Coleccionables y Juguetes",
+        enlace:"./index3.html#producto-3",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-marvel/Spiderman%20Traje%20Negro%20Y%20Dorado%20%23921.jpg",
         existencia: true
     },
@@ -203,6 +227,7 @@ let todofunco   = [
         nombre: "Figura De Accion Funko Pop Deluxe Marvel Avengers Infinity War Guardians Ship Star Lord #1021",
         Precio: "Q329.90",
         Franquicia:"Marvel",
+        enlace:"./index3.html#producto-4",
         descripcion: "Figuras Coleccionables",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-marvel/Star%20Lord.jpg",
         existencia: true
@@ -211,6 +236,7 @@ let todofunco   = [
         nombre: "Figura De Accion Funko Pop Marvel Thor Love And Thunder Gorr #1043",
         Precio: "Q149.90",
         Franquicia:"Marvel",
+        enlace:"./index3.html#producto-5",
         descripcion: "Figuras Coleccionables",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-marvel/Thunder%20Gorr%20%231043.jpg",
         existencia: true
@@ -219,6 +245,7 @@ let todofunco   = [
         nombre: "Figura De Accion Funko Pop Marvel Poison Spiderman #966",
         Precio: "Q169.90",
         Franquicia:"Marvel",
+        enlace:"./index3.html#producto-6",
         descripcion: "Figuras Coleccionables",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-marvel/Poison%20Spiderman.jpg",
         existencia: true
@@ -227,6 +254,7 @@ let todofunco   = [
         nombre: "Figura De Accion Funko POP #1023 Avengers Infinity War Guardians Ship Drax",
         Precio: "Q193.20",
         Franquicia:"Marvel",
+        enlace:"./index3.html#producto-7",
         descripcion: "Figuras Coleccionables",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-marvel/Ship%20Drax.jpg",
         existencia: true
@@ -235,6 +263,7 @@ let todofunco   = [
         nombre: "Figura De Accion Funko POP #1033 Doctor Strange In The Multiverse Of Madness Captain Carter",
         Precio: "Q159.90",
         Franquicia:"Marvel",
+        enlace:"./index3.html#producto-8",
         descripcion: "Figuras Coleccionables",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-marvel/Captain%20Carter.jpg",
         existencia: true
@@ -243,6 +272,7 @@ let todofunco   = [
         nombre: "Figura De Accion Funko POP #971 What If? Queen General Romanda",
         Precio: "Q159.90",
         Franquicia:"Marvel",
+        enlace:"./index3.html#producto-9",
         descripcion: "Figuras Coleccionables",
         img: "https://raw.githubusercontent.com/AxelAdal/Tienda/main/img/img-marvel/S3-%20POP%204.jpg",
         existencia: true
@@ -291,6 +321,18 @@ function displayResults(figuras) {
         <p>${figura.descripcion}</p>
         <p>Precio: ${figura.Precio}</p>
       `;
+
+      const button = document.createElement('button');
+      button.textContent = 'Ver detalles';
+      button.addEventListener('click', function() {
+        // Cambiar el enlace a la URL deseada para cada figura
+        const enlace = figura.enlace; // Reemplaza con el enlace correspondiente a la figura
+
+        // Redirigir a la URL especificada
+        window.location.href = enlace;
+      });
+
+      figuraElement.appendChild(button);
       resultsContainer.appendChild(figuraElement);
     });
   } else {
